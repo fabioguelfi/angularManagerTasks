5a5797d59a65b5832a6b9f52cbddda20146c4dde
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-04-26 08:39:59 -0300

    Implementando a listagem de tarefas - parte 1/2

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,11 +1,16 @@
-import { TarefaService } from './shared/tarefa.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { ListarTarefaComponent } from './listar';
+import { TarefaService } from './shared/tarefa.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   declarations: [
     ListarTarefaComponent
