da001ce7f35d2f53c68c9ade2d812fdf6c29cfdf
fabioguelfi <fabioguelfiunix@gmail.com>
2018-04-28 00:04:06 -0300

    Alterando o status de uma tarefa

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -17,10 +17,13 @@ export class ListarTarefaComponent implements OnInit {
   }
 
   public listarTodos(): Tarefa[] {
+
     return this.tarefaService.listarTodos();
+
   }
 
   public remover($event: any, tarefa: Tarefa): void {
+
     $event.preventDefault();
     if (confirm(`Deseja remover a tarefa ${tarefa.nome} ?`)) {
       this.tarefaService.remover(tarefa.id);
@@ -28,5 +31,18 @@ export class ListarTarefaComponent implements OnInit {
     }
 
   }
+
+  public alterarStatus(tarefa: Tarefa): void {
+
+    if (confirm(`Deseja Alterar o status da tarefa ${tarefa} ?`)) {
+
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.listarTodos();
+
+    }
+
+  }
+
+
 
 }
